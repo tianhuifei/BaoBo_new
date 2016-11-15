6834c93bd38ed5e22cd9d4c206770e0b30fec6d5
tianhuifeis@126.com <tianhuifeis@126.com>
2016-11-15 22:23:19 +0800

    修改

--- js/common.js
+++ js/common.js
@@ -1,54 +1,39 @@
-var CENTRT = "#$*"; //心中内容
+var CENTRT = "#$*"; //心跳内容
 mui.os.android ? os = "Webandroid" : os = "WebiPhone";
 /**
  * 处理服务器返回的数据，并且返回
  * @param {Object} re 服务器返回的DATA
  * @return {String} result 返回经过处理的数据
  */
-function dataDispose (re){
+function dataDispose (re,ws){
 	var result = re;
 	switch (result){
 		case "#$*":
 			//心跳，不做处理，直接返回
+			if(ws.readyState == ws.OPEN){
+				console.log(ws)
+				ws.send("#$*");
+			}
+			result = 0;
 			break;
 		default:
 			result = JSON.parse(result.substring(1,result.length-1)); // 去头，去尾获取返回对象
 			break;
 	}
 	
+	
 	return result;
 }
 
 /**
- * uuid
- * @param {Object} len
- * @param {Object} radix
+ * 错误信息提示
+ * @param {Object} data
  */
-function uuid(len, radix) {
-    var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
-    var uuid = [], i;
-    radix = radix || chars.length;
- 
-    if (len) {
-      // Compact form
-      for (i = 0; i < len; i++) uuid[i] = chars[0 | Math.random()*radix];
-    } else {
-      // rfc4122, version 4 form
-      var r;
- 
-      // rfc4122 requires these characters
-      uuid[8] = uuid[13] = uuid[18] = uuid[23] = '-';
-      uuid[14] = '4';
- 
-      // Fill in random data.  At i==19 set the high bits of clock sequence as
-      // per rfc4122, sec. 4.1.5
-      for (i = 0; i < 36; i++) {
-        if (!uuid[i]) {
-          r = 0 | Math.random()*16;
-          uuid[i] = chars[(i == 19) ? (r & 0x3) | 0x8 : r];
-        }
-      }
-    }
- 
-    return uuid.join('');
+function errorDispose (data){
+	var result  = true;
+	if( parseInt(data.errno)  == -1){// error:0      成功     error:-1失败
+		mui.toast(data.errorMsg);
+		result = false;
+	}
+	return result;
 }
